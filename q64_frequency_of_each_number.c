74d24ddb9fcb6111870ece3fd8f78f2fe8bea791
Harsh <hmehta0123@gmail.com>
2026-08-18 17:47:11 +0530

    find frequency of each elements in 1D array
    - Implemented linear frequency count using dynamic count array
    - Processed elements with direct indexing and duplicate suppression
    - Added dynamic allocation for result array returning element-frequency pairs
    - Verified O(N) time complexity and memory cleanup"

---
+++ q64_frequency_of_each_number.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* frq_of_each(int arr[],int arr_size , int *return_size) {
+
+    int count[10] = {0};
+
+    for (int i=0 ; i<arr_size;i++) {
+
+        count[arr[i]]++;
+    }
+
+    int *ans = (int*)malloc(arr_size*sizeof(int));
+
+    int k=0;
+
+
+    for (int i=0 ; i<arr_size;i++) {
+
+        if (count[arr[i]] >0) {
+
+            ans[k] = arr[i];
+
+            k++;
+
+            ans[k] = count[arr[i]];
+
+            k++;
+
+            count[arr[i]] = 0;
+
+
+        }
+
+    }
+
+
+    *return_size = k;
+
+    return ans;
+
+
+
+}
+
+
+
+
+
+int main() {
+
+    int arr[] = {2, 3, 2, 5, 3, 3, 8, 2};
+
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    int ans_size;
+
+    int* result = frq_of_each(arr,arr_size,&ans_size);
+
+    for (int i=0 ; i<ans_size ;i+=2) {
+
+        printf("Element: %d | Freq: %d\n", result[i], result[i+1]);
+
+    }
+
+    free(result);
+
+    return 0;
+}
